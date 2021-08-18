ca0e3971f935df57d30429190166f7d5ccd6422d
Eliseobas <89166566+Eliseobas@users.noreply.github.com>
2021-08-18 23:15:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-- 👋 Hi, I’m @Eliseobas
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
+import Discord
 
-<!---
-Eliseobas/Eliseobas is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+client = discord.Client()
+client.run(jFavweI-yq0v-kPv5K-juvfXTeBs0qYR)
